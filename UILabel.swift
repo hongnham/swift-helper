0b645280eecaf2f6e58efa769d4424121b6dee9f
pham hong nham <70361369+hongnham@users.noreply.github.com>
2021-06-18 14:27:39 +0700

    Create UILabel.swift

---
+++ UILabel.swift
@@ -0,0 +1,13 @@
+extension UILabel {
+    func setMargins(_ margin: CGFloat = 10) {
+        if let textString = self.text {
+            var paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.firstLineHeadIndent = margin
+            paragraphStyle.headIndent = margin
+            paragraphStyle.tailIndent = -margin
+            let attributedString = NSMutableAttributedString(string: textString)
+            attributedString.addAttribute(.paragraphStyle, value: paragraphStyle, range: NSRange(location: 0, length: attributedString.length))
+            attributedText = attributedString
+        }
+    }
+}
